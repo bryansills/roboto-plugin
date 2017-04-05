14a20d5193871b28c9362d2b1c05c37942aa2228
Bryan Sills <bryan@bryansills.com>
2017-04-04 21:52:45 -0500

    this is all messed up. lets try again.

--- gradle-hello/src/main/java/ninja/bryansills/HelloTask.java
+++ gradle-hello/src/main/java/ninja/bryansills/HelloTask.java
@@ -1,11 +1,15 @@
 package ninja.bryansills;
 
 import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.TaskAction;
 
 public class HelloTask extends DefaultTask {
+    @Input
+    public String who = "mate";
+
     @TaskAction
     public void helloTask() {
-        System.out.println("Hello world!");
+        System.out.println("Hello " + who + "!");
     }
 }
